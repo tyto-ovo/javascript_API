1f2819305fec4298f3395f63295b27e9e8b076f8
tyto-ovo <sjollyparker@gmail.com>
2024-09-01 22:28:03 -0400

    grafica lista

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -9,31 +9,93 @@ async function getCoin() {
     console.error("Error:", error);
   }
 }
+/* try {
+
+} catch (error) {
+  console.error("Error:", error);
+} */
+async function getPagina(moneda) {
+  try {
+    const res = await fetch(`https://mindicador.cl/api/${moneda}/`);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 // ------------ //
 
 async function obtDatosGrafica(moneda) {
-  const data = await getCoin();
-  console.log(moneda);
+  try {
+    const data = await getPagina(moneda);
+    const valuesT = [];
+    const xValues = [];
+    const yValues = [];
+    for (let i = 0; i < 10; i++) {
+      xValues.unshift(data.serie[i].fecha);
+      yValues.unshift(data.serie[i].valor);
+    }
+    valuesT.push(xValues);
+    valuesT.push(yValues);
+    return valuesT;
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
-obtDatosGrafica("uf");
+
+async function renderGrafica(moneda) {
+  const data = await obtDatosGrafica(moneda);
+  const config = {
+    type: "line",
+    data: {
+      labels: data[0],
+      datasets: [
+        {
+          label: "Historial de los últimos 10 días",
+          backgroundColor: "rgba(0,0,255,1.0)",
+          borderColor: "rgba(0,0,255,0.1)",
+          data: data[1],
+        },
+      ],
+    },
+  };
+
+  const divGrafica = document.querySelector(".grafica");
+  divGrafica.innerHTML = "";
+  const myChart = document.createElement("canvas");
+  myChart.id = "myChart";
+  myChart.style.backgroundColor = "white";
+  new Chart(myChart, config);
+  divGrafica.appendChild(myChart);
+}
+
 // ------------ //
 // FUNCION QUE APLICA PROMESA  //
 async function obtUf() {
-  const y = await getCoin();
-  /*   console.log(y["dolar"]["valor"]); */
-  return y["uf"]["valor"];
+  try {
+    const y = await getCoin();
+    return y["uf"]["valor"];
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
 
 async function obtDolar() {
-  const y = await getCoin();
-  /*   console.log(y["dolar"]["valor"]); */
-  return y["dolar"]["valor"];
+  try {
+    const y = await getCoin();
+    return y["dolar"]["valor"];
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
 
 async function obtEuro() {
-  const y = await getCoin();
-  /*   console.log(y["euro"]["valor"]); */
-  return y["euro"]["valor"];
+  try {
+    const y = await getCoin();
+    return y["euro"]["valor"];
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
 
 // ------------ //
@@ -65,21 +127,24 @@ async function calcular() {
   const tipoMoneda = document.getElementById("coin-select").value;
   const num = monedaChilena.value;
   let tipoCambio;
-
-  if (tipoMoneda == "uf") {
-    tipoCambio = await obtUf();
-    d = formatClp.format((num / tipoCambio).toFixed(2));
-    resultado.innerHTML = `Resultado: ${d} UF `;
-  } else if (tipoMoneda == "dolar") {
-    tipoCambio = await obtDolar();
-    d = formatUsd.format((num / tipoCambio).toFixed(2));
-    resultado.innerHTML = `Resultado: ${d} `;
-  } else if (tipoMoneda == "euro") {
-    tipoCambio = await obtEuro();
-    d = formatEu.format((num / tipoCambio).toFixed(2));
-    resultado.innerHTML = `Resultado: ${d}`;
+  try {
+    if (tipoMoneda == "uf") {
+      tipoCambio = await obtUf();
+      d = formatClp.format((num / tipoCambio).toFixed(2));
+      resultado.innerHTML = `Resultado: ${d} UF `;
+    } else if (tipoMoneda == "dolar") {
+      tipoCambio = await obtDolar();
+      d = formatUsd.format((num / tipoCambio).toFixed(2));
+      resultado.innerHTML = `Resultado: ${d} `;
+    } else if (tipoMoneda == "euro") {
+      tipoCambio = await obtEuro();
+      d = formatEu.format((num / tipoCambio).toFixed(2));
+      resultado.innerHTML = `Resultado: ${d}`;
+    }
+    renderGrafica(tipoMoneda);
+  } catch (error) {
+    console.error("Error:", error);
   }
-  /*   resultado.innerHTML = `Resultado: $${num / tipoCambio}`; */
 }
 
 ejecutar.addEventListener("click", calcular);
